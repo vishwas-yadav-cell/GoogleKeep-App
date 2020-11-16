8d9fadfcd72a9ce489073a44e90ed362d7807b62
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-11-16 18:07:51 +0530

    Updates

---
+++ precache-manifest.337d92ba985c07415ac5c7cc6279c847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ab3459518b0c92d8fe3e27a7742718",
+    "url": "/GoogleKeep-App/index.html"
+  },
+  {
+    "revision": "2382338e9dec52ddca2c",
+    "url": "/GoogleKeep-App/static/css/main.9702484f.chunk.css"
+  },
+  {
+    "revision": "4c4b85cc56233f94fd03",
+    "url": "/GoogleKeep-App/static/js/2.513ea39f.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/GoogleKeep-App/static/js/2.513ea39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2382338e9dec52ddca2c",
+    "url": "/GoogleKeep-App/static/js/main.279b2c4d.chunk.js"
+  },
+  {
+    "revision": "2182af2f3ef2e25ad9f5",
+    "url": "/GoogleKeep-App/static/js/runtime-main.08a670a8.js"
+  }
+]);
